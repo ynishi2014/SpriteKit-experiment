01d4eb656e1acf7300be0850eccebfc3b5dad768
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2014-10-10 09:42:45 +0900

    Limit sprite number to five
    Store generate sprites to spriteArray
    Delete sprite if the number of sprite is over five
    
    Delete Hello, World!
    Make sprites smaller.

--- Game/GameScene.swift
+++ Game/GameScene.swift
@@ -9,16 +9,13 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
+
+	var spriteArray:[SKSpriteNode] = []
+
+	override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
     }
-    
+	
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
@@ -27,15 +24,22 @@ class GameScene: SKScene {
             
             let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
+            sprite.xScale = 0.2
+            sprite.yScale = 0.2
             sprite.position = location
             
             let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
             sprite.runAction(SKAction.repeatActionForever(action))
+			spriteArray.append(sprite);
             
             self.addChild(sprite)
+			
+			println("sprite" + spriteArray.count.description)
+			if spriteArray.count > 5 {
+				spriteArray[0].removeFromParent()
+				spriteArray.removeAtIndex(0)
+			}
         }
     }
    
